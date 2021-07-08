41f5fe088a546f42196040bb5d2d2a62a6552424
kirsten tay <taykirsten11@gmail.com>
2021-07-08 22:39:06 +0000

    added the cryptography

---
+++ src/components/Sec.jsx
@@ -0,0 +1,194 @@
+import React from "react";
+
+
+
+ function Sec() {
+  return (
+    <>
+      <div className="crptograph">
+        <p className="title">Create your cryptocurrency portfolio today</p>
+        <p className="title_btm">
+          Coinbase has a variety of features that make it the best place to
+          start trading
+        </p>
+        <div className="section">
+          <div className="left">
+            <div className="row_port">
+              <i className="mx-4 shadow fa fa-camera-retro fa-2x"></i>
+              <div>
+                <p className="titile bold my-2">Manage your portfolio</p>
+                <p className="desc">
+                  Buy and sell popular digital currencies, keep track of them in
+                  the one place.
+                </p>
+              </div>
+            </div>
+            <div className="row_port">
+              <i className="mx-4 shadow fa fa-camera-retro fa-2x"></i>
+              <div>
+                <p className="titile bold my-2">Manage your portfolio</p>
+                <p className="desc">
+                  Buy and sell popular digital currencies, keep track of them in
+                  the one place.
+                </p>
+              </div>
+            </div>
+            <div className="row_port">
+              <i className="mx-4 shadow fa fa-camera-retro fa-2x"></i>
+              <div>
+                <p className="titile bold my-2">Manage your portfolio</p>
+                <p className="desc">
+                  Buy and sell popular digital currencies, keep track of them in
+                  the one place.
+                </p>
+              </div>
+            </div>
+            <div className="row_port">
+              <i className="mx-4 shadow fa fa-camera-retro fa-2x"></i>
+              <div>
+                <p className="titile bold my-2">Manage your portfolio</p>
+                <p className="desc">
+                  Buy and sell popular digital currencies, keep track of them in
+                  the one place.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <img
+              src="https://assets.coinbase.com/assets/coinbase-app.3b0bfd4cb6b7a7614c1e18472187f6b9.webp"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="platform">
+        <p className="title">The most trusted cryptocurrency platform</p>
+        <p className="title_btm mt-2 mb-5">
+          Here are a few reasons why you should choose Coinbase
+        </p>
+        <div className="sec">
+          <div className="pat px-3">
+            <div className="imgc">
+              <img
+                src="https://d3q1dtqmzkinuf.cloudfront.net/website-data/index/tsaw_index_no_pandrive.svg"
+                alt=""
+              />
+            </div>
+            <p className="my-4 title">Secure Storage</p>
+            <p className="my-4 desc">
+              We store the vast majority of the digital assets in secure offline
+              storage.
+            </p>
+            <a href="#">
+              Learn how Coinbase keeps your funds safe and secure >
+            </a>
+          </div>
+          <div className="pat px-3">
+            <div className="imgc">
+              <img
+                src="https://d3q1dtqmzkinuf.cloudfront.net/website-data/index/tsaw_index_no_pandrive.svg"
+                alt=""
+              />
+            </div>
+            <p className="my-4 title">Secure Storage</p>
+            <p className="my-4 desc">
+              We store the vast majority of the digital assets in secure offline
+              storage.
+            </p>
+            <a href="#">
+              Learn how Coinbase keeps your funds safe and secure >
+            </a>
+          </div>
+          <div className="pat px-3">
+            <div className="imgc">
+              <img
+                src="https://d3q1dtqmzkinuf.cloudfront.net/website-data/index/tsaw_index_no_pandrive.svg"
+                alt=""
+              />
+            </div>
+
+            <p className="my-4 title">Secure Storage</p>
+            <p className="my-4 desc">
+              We store the vast majority of the digital assets in secure offline
+              storage.
+            </p>
+            <a href="#">
+              Learn how Coinbase keeps your funds safe and secure >
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="counter">
+        <div className="part">
+          <p className="title">$335B</p>
+          <p className="title_btm">Quarterly volume traded</p>
+        </div>
+        <div className="part">
+          <p className="title">$335B</p>
+          <p className="title_btm">Quarterly volume traded</p>
+        </div>
+        <div className="part">
+          <p className="title">$335B</p>
+          <p className="title_btm">Quarterly volume traded</p>
+        </div>
+      </div>
+
+      <div className="platform my-4">
+        <p className="title">Get started in a few minutes</p>
+        <p className="title_btm mt-2 mb-5">
+          Coinbase supports a variety of the most popular digital currencies.
+        </p>
+        <div className="sec">
+          <div className="pat mx-4">
+            <div className="imgc">
+              <img
+                src="https://d3q1dtqmzkinuf.cloudfront.net/website-data/index/tsaw_index_no_pandrive.svg"
+                alt=""
+              />
+            </div>
+            <p className="my-4 title">Create an account</p>
+          </div>
+          <div className="pat mx-4">
+            <div className="imgc">
+              <img
+                src="https://d3q1dtqmzkinuf.cloudfront.net/website-data/index/tsaw_index_no_pandrive.svg"
+                alt=""
+              />
+            </div>
+            <p className="my-4 title">Link your bank account</p>
+          </div>
+          <div className="pat mx-4">
+            <div className="imgc">
+              <img
+                src="https://d3q1dtqmzkinuf.cloudfront.net/website-data/index/tsaw_index_no_pandrive.svg"
+                alt=""
+              />
+            </div>
+
+            <p className="my-4 title">Secure Storage</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="earth">
+        <div className="left">
+          <p className="blod_t">Earn up to $28 worth of crypto</p>
+          <p className="py-1">Earn up to $28 worth of crypto</p>
+          <button style={{ background: "#1652F0" }} className="btn btn-primary">
+            Start Earning
+          </button>
+        </div>
+        <div className="right">
+          <img
+            src="https://assets.coinbase.com/assets/earn-upsell-desktop.dad0f473399e16fa8c176a9599c883ea.webp"
+            alt=""
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+export default Sec;
